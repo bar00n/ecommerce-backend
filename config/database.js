756bfa2d50852db4ae46b71734f1cb4d132c1db3
Barun Hafidz <barunhafidz7@gmail.com>
2021-01-14 06:34:45 +0700

    fix2 db

--- config/database.js
+++ config/database.js
@@ -1,30 +1,30 @@
 const parse = require('pg-connection-string').parse;
 
-if(env('NODE_ENV') === 'production') {
-  const config = parse(process.env.DATABASE_URL)
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'postgres',
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          username: config.user,
-          password: config.password
-        },
-        options: {
-          ssl: false
-        } 
+module.exports = ({ env }) => {
+  
+  if(env('NODE_ENV') === 'production') {
+    const config = parse(process.env.DATABASE_URL)
+      return {
+        defaultConnection: 'default',
+        connections: {
+          default: {
+            connector: 'bookshelf',
+            settings: {
+              client: 'postgres',
+              host: config.host,
+              port: config.port,
+              database: config.database,
+              username: config.user,
+              password: config.password
+            },
+            options: {
+              ssl: false
+            } 
+          }
+        }
       }
     }
-  }
-}
 
-
-module.exports = ({ env }) => {
   return {
   defaultConnection: 'default',
   connections: {
