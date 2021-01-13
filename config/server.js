01119534010169c5cac04e3aeb4c1061dbde4da6
Barun Hafidz <barunhafidz7@gmail.com>
2021-01-13 18:28:27 +0700

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '55ba6cd055f48dfc64e18801b428729e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a07b4d51-2275-4ac0-9013-d97fda5064ef'
+};
